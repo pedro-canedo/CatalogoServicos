31717213b0d0ce0f0085c53fcbeb1db2a6bf2a02
Pedro Augusto Canedo Araújo Obalhe <pedro.obalhe@conveste.com.br>
2022-12-03 03:00:19 -0300

    Finalizando projeto!

--- CatalogoServicos/settings.py
+++ CatalogoServicos/settings.py
@@ -77,28 +77,30 @@ WSGI_APPLICATION = 'CatalogoServicos.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':'db',  # postgres
-        'USER':'postgres',  # postgres
-        'PASSWORD':'postgres',  # postgres
-        'HOST':'localhost',  # localhost
-        'PORT':5433,
-    }
-}
-
-
+#rodar sem compose com está configuração
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME':'postgres',  # postgres
+#         'NAME':'db',  # postgres
 #         'USER':'postgres',  # postgres
 #         'PASSWORD':'postgres',  # postgres
-#         'HOST':'db',  # localhost
-#         'PORT':5432,
+#         'HOST':'localhost',  # localhost
+#         'PORT':5433,
 #     }
 # }
+
+
+#com compose nesta configuração
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'postgres',  # postgres
+        'USER':'postgres',  # postgres
+        'PASSWORD':'postgres',  # postgres
+        'HOST':'db',  # localhost
+        'PORT':5432,
+    }
+}
 
 
 # Password validation
